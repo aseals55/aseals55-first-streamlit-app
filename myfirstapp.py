bbaac2cea3bbbc91f180cf41aad07c35620cbc7b
Alline Seals <108949382+aseals55@users.noreply.github.com>
2023-11-09 14:46:06 -0600

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -40,20 +40,22 @@ try:
 except URLError as e:
   streamlit.error()
 
+streamlit.header("The fruit load list contains:")
+#Snowflake related functions
+def get_fruit_load_list():
+ with my_cnx.cursor() as my_cur:
+      my_cur.execute("select * from fruit_load_list")
+      return my_cur.fetchall()
 
-
-
-
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#streamlit.write('The user entered ', fruit_choice)
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-#streamlit.dataframe(fruityvice_normalized)
+#Add a button to load the fruit
+if streamlit.button('Get fruit load list'):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])  
+   my_data_row = get_fruit_load_list()
+   streamlit.dataframe(my_data_row)
 
 streamlit.stop()
+
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -66,4 +68,13 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 #streamlit.write('Thanks for adding', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#streamlit.text(fruityvice_response.json())
+# write your own comment -what does the next line do? 
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+#streamlit.dataframe(fruityvice_normalized)
