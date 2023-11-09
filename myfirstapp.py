68926b77099dbb25103a5482f6a81dc14114246f
Alline Seals <108949382+aseals55@users.noreply.github.com>
2023-11-09 14:29:57 -0600

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -30,12 +30,12 @@ def get_fruityvice_data(this_fruit_choice):
   
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'this_fruit_choice')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.error('Please select a fruit to get information.')
+    streamlit.error("Please select a fruit to get information.")
   else:
     back_from_funtion = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
+    streamlit.dataframe(back_from_funtion)
 
 except URLError as e:
   streamlit.error()
