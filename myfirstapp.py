3161bf7dfd032bffbe368de3cb8183e212217e28
Alline Seals <108949382+aseals55@users.noreply.github.com>
2023-11-09 14:23:13 -0600

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -30,7 +30,7 @@ def get_fruityvice_data(this_fruit_choice):
   
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?',this_fruit_choice)
+  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'this_fruit_choice')
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
